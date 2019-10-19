fa0521d97e6b6e7018977a5f60c87b0d002eea5c
さっくん <mo-takusan9922@docomo.ne.jp>
2019-10-19 16:32:06 +0900

    create or-elm module

---
+++ or_elm.py
@@ -0,0 +1,161 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+class OS_ELM_Base:
+    def __init__(self, in_shape, hidden_shape, out_shape, constant, forget_fact=1, reccurent=False):
+        """
+            Input:
+                constant : 1 / C
+                forget_fact: in (0, 1]
+        """
+        def iden(shape):
+            return tf.diag(tf.ones((1, shape)))
+
+        in_shape = list(in_shape)
+        hidden_shape = list(hidden_shape)
+        out_shape = list(out_shape)
+
+        self.input = tf.placeholder(tf.float32, shape=[None]+in_shape)
+        self.true = tf.placeholder(tf.float32, shape=[None]+out_shape)
+
+        self.w = tf.Variable(
+            tf.truncated_normal(
+                shape=hidden_shape+in_shape, 
+                mean=0.0, 
+                stddev=1.0, 
+                dtype=tf.float32))
+        self.b = tf.Variable(
+            tf.truncated_normal(
+                shape=hidden_shape, 
+                mean=0.0, 
+                stddev=1.0, 
+                dtype=tf.float32))
+
+        self.new_w = tf.placeholder(tf.float32, shape=hidden_shape+in_shape)
+        self.update_w = tf.assign(self.w, self.new_w)
+
+        mat = tf.matmul(self.input, self.w) + self.b 
+
+        self.reccurent = reccurent
+        if reccurent:
+            self.v = tf.Variable(
+                tf.truncated_normal(
+                    shape=hidden_shape+hidden_shape, 
+                    mean=0.0, 
+                    stddev=1.0, 
+                    dtype=tf.float32))
+            self.new_v = tf.placeholder(tf.float32, shape=hidden_shape+hidden_shape)
+            self.update_v = tf.assign(self.v, self.new_v)
+            self.h = mat + self.v
+        else:
+            self.h = mat
+
+        self.p = tf.Variable(
+            tf.zeros(shape=hidden_shape+hidden_shape))
+
+        self.p0 = tf.assign(self.p,
+            tfp.math.pinv(tf.matmul(self.h, self.h, transpose_a=True) + constant * iden(hidden_shape[0]))
+        )
+
+        self.beta = tf.Variable(
+            tf.zeros(shape=hidden_shape+out_shape))
+
+        self.beta0 = tf.assign(self.beta,
+            tf.matmul(
+                tf.matmul(self.p0, self.h, transpose_b=True), 
+                self.true)
+        )
+
+        self.output = tf.matmul(self.h, self.beta)
+
+        self.update_p = tf.assign(self.p,
+             1 / forget_fact * self.p - tf.matmul(
+                tf.matmul(
+                    tf.matmul(
+                        tf.matmul(self.p, self.h, transpose_b=True),
+                        tfp.math.pinv(forget_fact * forget_fact * iden(in_shape[0]) + tf.matmul(
+                            tf.matmul(
+                                self.h, 
+                                self.p), 
+                            self.h, transpose_b=True))),
+                    self.h),
+                self.p)
+        )
+
+        self.update_beta = tf.assign(self.beta,
+            self.beta + tf.matmul(
+                tf.matmul(self.update_p, self.h, transpose_b=True),
+                (self.true - tf.matmul(self.h, self.beta)))
+        )
+
+    def init_beta(self, x, y, session):
+        session.run(self.beta0, feed_dict={
+            self.input : x,
+            self.true  : y})
+
+    def train(self, x, y, session, can_apply_trans_beta=False):
+        beta = session.run(self.update_beta, feed_dict={
+            self.input : x,
+            self.true  : y})
+        
+        if can_apply_trans_beta:
+            session.run(self.update_w, feed_dict={
+                self.new_w : beta.T})
+
+    def assign_weight(self, w, session):
+        session.run(self.update_w, feed_dict={
+            self.new_w : w})
+
+class OS_ELM(OS_ELM_Base):
+    def __init__(self, in_shape, hidden_shape, out_shape, constant, forget_fact=1):
+        super().__init__(
+            in_shape, 
+            hidden_shape, 
+            out_shape, 
+            constant, 
+            forget_fact, 
+            False)
+
+
+class OS_ELM_Rec(OS_ELM_Base):
+    def __init__(self, in_shape, hidden_shape, out_shape, constant, forget_fact=1):
+        super().__init__(
+            in_shape, 
+            hidden_shape, 
+            out_shape, 
+            constant, 
+            forget_fact, 
+            True)
+
+    def assign_hidden_weight(self, v, session):
+        session.run(self.update_v, feed_dict={
+            self.new_v : v})
+
+
+class OR_ELM:
+    def __init__(self, in_shape, hidden_shape, out_shape, constant, forget_fact=1, config=None):
+        """
+            Input:
+                constant : 1 / C
+                forget_fact: in (0, 1]
+        """
+        self.elmrc = OS_ELM_Rec(in_shape, hidden_shape, out_shape, constant, forget_fact)
+        self.elm_ae_iw = OS_ELM(in_shape, hidden_shape, in_shape, constant, forget_fact)
+        self.elm_ae_hw = OS_ELM(hidden_shape, hidden_shape, hidden_shape, constant, forget_fact)
+        self.session = tf.Session(config=config)
+
+    def init_model(self, x, y):
+        self.elmrc.init_beta(x, y, self.session)
+        self.elm_ae_iw.init_beta(x, y, self.session)
+        self.elm_ae_hw.init_beta(x, y, self.session)
+
+    def train(self, x, y):
+        h = self.elmrc.h
+        self.elm_ae_hw.train(x, x, self.session, can_apply_trans_beta=True)
+        self.elm_ae_iw.train(h, h, self.session, can_apply_trans_beta=True)
+
+        self.elmrc.assign_hidden_weight(self.elm_ae_hw.v, self.session)
+        self.elmrc.assign_weight(self.elm_ae_iw.w, self.session)
+        self.elmrc.train(x, y, self.session, can_apply_trans_beta=False)
+
