edd747ffad8e08ccc6761acb86176e27c71109bb
さっくん <mo-takusan9922@docomo.ne.jp>
2019-10-25 14:15:51 +0900

    change autoencoder

--- auto_encoder.py
+++ auto_encoder.py
@@ -1,30 +1,184 @@
 #%%
+import changefinder 
+import matplotlib.pyplot as plt
 import numpy as np
 import os
+import pandas as pd
 import sys
 import tensorflow as tf
+from sklearn import metrics
 sys.path.append('/home/sakuma/work/python/OR-ELM-tensorflow/')
 
 import or_elm as elm
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 #%%
-if __name__ == '__main__':
-    y = np.load('data/y_pose.npy')[:1000]
-    X = np.load('data/x_pose.npy')[:1000]
-    
-    X_0 = X[y==6]
-    X_1 = X[y==5]
+"""
+    この学習方法では則適応する
+    機構的にオートエンコーダにするのは正しいのか…？
+        - 現在のデータと過去のデータから現在のデータを復元ってちょっとおかしい気がする
+        - 次の出力を予測するほうがRNNらしい気が、、、？
 
-    del X
-    del y
 
-    # n * T * C
-    in_shape = X_0.shape[2]
-    print("shape:", X_0.shape)
-    model = elm.OR_ELM([in_shape], [in_shape // 4], [in_shape])
-    for x in X_0:
-        loss = model.loss(x, x)
-        print(f"\rloss:{loss}", end="")
-        model.train(x, x)
-    
+    ★そもそも異常検知をどのように評価するのか知る必要がありそう
+"""
+X = np.load('data/x_pose_BackwardWalk.npy')
+X_f = np.load('data/x_pose_Walking.npy')
+
+# n * T * C
+in_shape = X.shape[2]
+print("shape:", X.shape)
+model = elm.OR_ELM([in_shape], [in_shape // 4], [in_shape], forget_fact=1)
+i = 0
+for xfs in X_f:
+    for xs in X:
+        for x in xs:
+            x = x[np.newaxis,:]
+            loss = model.loss(x, x)
+            print(f"\rbackward loss:{loss}", end="")
+            model.train(x, x)
+
+        i+= 1
+        if i % 1000 == 0:
+            print("")
+            for xf in xfs:
+                xf = xf[np.newaxis,:]
+                loss = model.loss(xf, xf)
+                print(f"\rforward loss:{loss}", end="")
+                model.train(xf, xf)
+
+            print("")
+model.close()
+
+#%%
+"""
+    次の値を予測するモデル
+    AUCで評価をとりたい
+    その場で異常を判断していいのか問題
+        - 
+"""
+X = np.load('data/x_pose_BackwardWalk.npy')
+X_f = np.load('data/x_pose_Walking.npy')
+
+def data(xs):
+    for x in xs:
+        yield x[np.newaxis,:]
+
+# n * T * C
+in_shape = X.shape[2]
+print("shape:", X.shape)
+model = elm.OR_ELM([in_shape], [in_shape // 4], [in_shape], forget_fact=1)
+i = 0
+for xfs in X_f:
+    for xs in X:
+        gen = data(xs)
+        nex = None
+        for x in gen:
+            if nex is not None: 
+                loss = model.loss(nex, x)
+                print(f"\rbackward loss:{loss}", end="")
+            
+            nex = next(gen, None)
+            # 次の値を予測するように学習する
+            if nex is not None:
+                model.train(x, nex)
+
+        i+= 1
+        if i % 1000 == 0:
+            print("")
+            gen = data(xfs)
+            nex = None
+            for xf in gen:
+                if nex is not None:
+                    loss = model.loss(xf, xf)
+                    print(f"\rforward loss:{loss}", end="")
+                
+                nex = next(gen, None)
+                if nex is not None:
+                    model.train(xf, xf)
+
+            print("")
+model.close()
+
+#%% change finder
+"""
+データが正規分布に従うか判定する必要がある
+＝ChangeFinderの仮定について正規分布が成り立つか確認
+
+EOF解析によって時系列データの主成分分析を行うことができるか
+"""
+
+
+
+
+
+
+
+
+#%%
+nX = np.loadtxt('/home/sakuma/work/python/OR-ELM-tensorflow/data/negative_data_0.csv', delimiter=',')
+X = np.loadtxt('/home/sakuma/work/python/OR-ELM-tensorflow/data/data_np_test_0.csv', delimiter=',')
+y = np.loadtxt('/home/sakuma/work/python/OR-ELM-tensorflow/data/label_np_test_0.csv', delimiter=',')
+
+#%%
+# T * C
+in_shape = X.shape[1]
+print("shape:", X.shape)
+model = elm.OR_ELM([in_shape], [in_shape // 4], [in_shape], forget_fact=1)
+
+cfs = [changefinder.ChangeFinder() for i in range(in_shape)]
+for x in nX:
+    map(lambda cf, x_n: cf.update(x_n), cfs, x)
+    x = x[np.newaxis,:]
+    model.train(x, x)
+
+losses = []
+scores = []
+for x in X:
+    score = sum([cf.update(x_n) for cf, x_n in zip(cfs, x)]) 
+    x = x[np.newaxis,:]
+    loss = model.loss(x, x)
+    print(f"\ror_elm loss:{loss} | cf score:{score}", end="")
+    model.train(x, x)
+
+    losses.append(loss)
+    scores.append(score)
+
+model.close()
+
+#%%
+"""
+これを見るとChangeFinderでは見つけることができない
+"""
+
+# FPR, TPR(, しきい値) を算出
+fpr, tpr, thresholds = metrics.roc_curve(np.where(y == 0, 0, 1), losses)
+
+# ついでにAUCも
+auc = metrics.auc(fpr, tpr)
+
+# ROC曲線をプロット
+plt.plot(fpr, tpr, label='ORELM (area = %.2f)'%auc)
+plt.legend()
+plt.title('ROC curve')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.grid(True)
+
+# FPR, TPR(, しきい値) を算出
+fpr, tpr, thresholds = metrics.roc_curve(np.where(y == 0, 0, 1), scores)
+
+# ついでにAUCも
+auc = metrics.auc(fpr, tpr)
+
+# ROC曲線をプロット
+plt.plot(fpr, tpr, label='Change Finder (area = %.2f)'%auc)
+plt.legend()
+plt.title('ROC curve')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.grid(True)
+
+plt.show()
+
+# %%
--- or_elm.py
+++ or_elm.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import tensorflow_probability as tfp
 
 class OS_ELM_Base:
-    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1, reccurent=False):
+    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1, reccurent=False, name=''):
         """
             Input:
                 constant : 1 / C
@@ -15,83 +15,79 @@ class OS_ELM_Base:
         def norm(y):
             mean, variance = tf.nn.moments(y, [1], keep_dims=True)
             return (y - mean) / tf.sqrt(variance + 1e-5)
-            
-        in_shape = list(in_shape)
-        hidden_shape = list(hidden_shape)
-        out_shape = list(out_shape)
-        batch_size = [batch_size]
+        
+        with tf.name_scope(name):
+            in_shape = list(in_shape)
+            hidden_shape = list(hidden_shape)
+            out_shape = list(out_shape)
+            batch_size = [batch_size]
 
-        self.input = tf.placeholder(tf.float32, shape=batch_size+in_shape)
-        self.true = tf.placeholder(tf.float32, shape=batch_size+out_shape)
+            self.input = tf.placeholder(tf.float32, shape=batch_size+in_shape, name='placeholder_input')
+            self.true = tf.placeholder(tf.float32, shape=batch_size+out_shape, name='placeholder_output')
 
-        self.w = tf.Variable(
-            tf.truncated_normal(
-                shape=in_shape+hidden_shape, 
-                mean=0.0, 
-                stddev=1.0, 
-                dtype=tf.float32))
-        self.b = tf.Variable(
-            tf.truncated_normal(
-                shape=[1]+hidden_shape, 
-                mean=0.0, 
-                stddev=1.0, 
-                dtype=tf.float32))
+            self.w = tf.Variable(
+                tf.truncated_normal(
+                    shape=in_shape+hidden_shape, 
+                    mean=0.0, 
+                    stddev=1.0, 
+                    dtype=tf.float32), name='variable_w')
+            self.b = tf.Variable(
+                tf.truncated_normal(
+                    shape=[1]+hidden_shape, 
+                    mean=0.0, 
+                    stddev=1.0, 
+                    dtype=tf.float32), name='variable_b')
 
-        self.new_w = tf.placeholder(tf.float32, shape=in_shape+hidden_shape)
-        self.update_w = tf.assign(self.w, self.new_w)
+            self.new_w = tf.placeholder(tf.float32, shape=in_shape+hidden_shape, name='placeholder_new_w')
+            self.update_w = tf.assign(self.w, self.new_w)
 
-        mat = tf.matmul(self.input, self.w) + tf.tile(self.b, batch_size+[1])
+            mat = tf.matmul(self.input, self.w) + tf.tile(self.b, batch_size+[1])
 
-        self.reccurent = reccurent
-        if reccurent:
-            # self.v = tf.Variable(
-            #     tf.truncated_normal(
-            #         shape=hidden_shape+hidden_shape, 
-            #         mean=0.0, 
-            #         stddev=1.0, 
-            #         dtype=tf.float32))
-            self.v = tf.placeholder(tf.float32, shape=hidden_shape+hidden_shape)
-            # self.update_v = tf.assign(self.v, self.new_v)
+            self.reccurent = reccurent
+            if reccurent:
+                self.v = tf.placeholder(tf.float32, shape=hidden_shape+hidden_shape, name='placeholder_v')
 
-            self.h = tf.Variable(tf.zeros(shape=batch_size+hidden_shape))
-            self.update_h = tf.assign(self.h,
-                tf.nn.relu(norm(mat + tf.matmul(self.h, self.v)))
-            )
-        else:
-            self.h = tf.nn.relu(norm(mat))
+                self.h = tf.Variable(tf.zeros(shape=batch_size+hidden_shape), name='variable_h')
+                self.update_h = tf.assign(self.h,
+                    tf.nn.relu(norm(mat + tf.matmul(self.h, self.v)))
+                )
+            else:
+                self.h = tf.nn.relu(norm(mat))
 
-        self.p = tf.Variable(constant * iden(hidden_shape[0]))
-        self.beta = tf.Variable(tf.zeros(shape=hidden_shape+out_shape))
+            self.p = tf.Variable(constant * iden(hidden_shape[0]), name='variable_p')
+            self.beta = tf.Variable(tf.zeros(shape=hidden_shape+out_shape), name='variable_beta')
 
-        self.output = tf.matmul(self.h, self.beta)
+            self.output = tf.matmul(self.h, self.beta)
 
-        self.update_p = tf.assign(self.p,
-            1 / forget_fact * self.p - tf.matmul(
-                tf.matmul(
-                    self.p,
-                    self.h, transpose_b=True),
-                tf.matmul(
-                    tfp.math.pinv(
-                        forget_fact * forget_fact * iden(batch_size[0]) + (
-                        forget_fact * tf.matmul(
-                            tf.matmul(
-                                self.h,
-                                self.p),
-                            self.h, transpose_b=True))),
+            self.inv = tfp.math.pinv(
+                            forget_fact * forget_fact * iden(batch_size[0]) + (
+                            forget_fact * tf.matmul(
+                                tf.matmul(
+                                    self.h,
+                                    self.p),
+                                self.h, transpose_b=True)))
+
+            self.update_p = tf.assign(self.p,
+                1 / forget_fact * self.p - tf.matmul(
                     tf.matmul(
-                        self.h,
-                        self.p)))
-        )
+                        self.p,
+                        self.h, transpose_b=True),
+                    tf.matmul(
+                        self.inv,
+                        tf.matmul(
+                            self.h,
+                            self.p)))
+            )
 
-        diff = self.true - self.output
+            diff = self.true - self.output
 
-        self.update_beta = tf.assign(self.beta,
-            self.beta + tf.matmul(
-                tf.matmul(self.update_p, self.h, transpose_b=True),
-                diff)
-        )
+            self.update_beta = tf.assign(self.beta,
+                self.beta + tf.matmul(
+                    tf.matmul(self.update_p, self.h, transpose_b=True),
+                    diff)
+            )
 
-        self.cost = tf.reduce_mean(tf.square(diff))
+            self.cost = tf.reduce_mean(tf.square(diff))
 
     def train(self, x, y, session, can_apply_trans_beta=False):
         beta = session.run(self.update_beta, feed_dict={
@@ -113,7 +109,7 @@ class OS_ELM_Base:
 
 
 class OS_ELM(OS_ELM_Base):
-    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1):
+    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1, name='os_elm'):
         super().__init__(
             in_shape, 
             hidden_shape, 
@@ -121,11 +117,12 @@ class OS_ELM(OS_ELM_Base):
             batch_size,
             constant, 
             forget_fact, 
-            False)
+            False,
+            name)
 
 
 class OS_ELM_Rec(OS_ELM_Base):
-    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1):
+    def __init__(self, in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact=1, name='os_elm_rec'):
         super().__init__(
             in_shape, 
             hidden_shape, 
@@ -133,12 +130,27 @@ class OS_ELM_Rec(OS_ELM_Base):
             batch_size,
             constant, 
             forget_fact, 
-            True)
+            True,
+            name)
 
     def update_hidden(self, v, session):
-        session.run(self.update_h, feed_dict={
+        session.run(self.v, feed_dict={
             self.v : v})
+
+    def train(self, x, y, session, v=None, can_apply_trans_beta=False):
+        if v is None:
+            super().train(x, y, session, can_apply_trans_beta)
+        else:
+            session.run(self.update_h, feed_dict={
+                self.input : x,
+                self.v     : v})
+
+            beta = session.run(self.update_beta, feed_dict={
+                self.true  : y})
             
+            if can_apply_trans_beta:
+                session.run(self.update_w, feed_dict={
+                    self.new_w : beta.T})
 
 class OR_ELM:
     def __init__(self, in_shape, hidden_shape, out_shape, batch_size=1, constant=1e-5, forget_fact=1, config=None):
@@ -148,18 +160,26 @@ class OR_ELM:
                 forget_fact: in (0, 1]
         """
         self.elmrc = OS_ELM_Rec(in_shape, hidden_shape, out_shape, batch_size, constant, forget_fact)
-        self.elm_ae_iw = OS_ELM(in_shape, hidden_shape, in_shape, batch_size, constant, forget_fact)
-        self.elm_ae_hw = OS_ELM(hidden_shape, hidden_shape, hidden_shape, batch_size, constant, forget_fact)
+        self.elm_ae_iw = OS_ELM(in_shape, hidden_shape, in_shape, batch_size, constant, forget_fact, 'elm_ae_iw')
+        self.elm_ae_hw = OS_ELM(hidden_shape, hidden_shape, hidden_shape, batch_size, constant, forget_fact, 'elm_ae_hw')
+
+        init_op = tf.global_variables_initializer()
         self.session = tf.Session(config=config)
+        self.session.run(init_op)
+
 
     def train(self, x, y):
-        h = self.elmrc.h
-        self.elm_ae_hw.train(x, x, self.session, can_apply_trans_beta=True)
-        self.elm_ae_iw.train(h, h, self.session, can_apply_trans_beta=True)
+        h = self.session.run(self.elmrc.h)
+        self.elm_ae_hw.train(h, h, self.session, can_apply_trans_beta=True)
+        self.elm_ae_iw.train(x, x, self.session, can_apply_trans_beta=True)
 
-        self.elmrc.update_hidden(self.elm_ae_hw.w, self.session)
-        self.elmrc.assign_weight(self.elm_ae_iw.w, self.session)
-        self.elmrc.train(x, y, self.session, can_apply_trans_beta=False)
+        v = self.session.run(self.elm_ae_hw.w)
+        w = self.session.run(self.elm_ae_iw.w)
+        self.elmrc.assign_weight(w, self.session)
+        self.elmrc.train(x, y, self.session, v, can_apply_trans_beta=False)
 
     def loss(self, x, y):
         return self.elmrc.loss(x, y, self.session)
+
+    def close(self):
+        self.session.close()
